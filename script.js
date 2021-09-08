8d74eec596cbe233473846042e8959a5ef6ad465
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-09-08 12:10:41 +0200

    books are divided int o two tables and can be added and deleted

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// TODO create two separate arrays for books read and books to read
 // array that will store all the book objects
 let myLibrary = [];
+let booksToRead = [];
+let booksRead = [];
 
 // constructor to create book objects
 function Book(title, author, pages, read) {
@@ -15,31 +18,48 @@ function addBookToLibrary() {
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    // check if the checkbox is on and display a proper message
+    // depending on the value of the checkbox add book to different array
     const checkbox = document.querySelector("#read");
     let read = "";
+    const newBook = new Book(title, author, pages, read);
     if (checkbox.checked) {
-        read = "Read";
+        newBook.read = "Read";
+        booksRead.push(newBook);
     }
     else {
-        read = "Not Read"
+        newBook.read = "Not Read";
+        booksToRead.push(newBook);
     }
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
 }
 
 // function for displaying the books in the table
-function displayBooks(myLibrary) {
-    const table = document.getElementById("display-books");
+function displayBooks(booksRead, booksToRead) {
+    let booksToReadPlaceholder = document.querySelector(".display-books-to-read");
+    let booksReadPlaceholder = document.querySelector(".display-books-read");
+
     // for every book in myLibrary array
-    for (let book of myLibrary) {
-        let bookIndex = myLibrary.indexOf(book);
+    for (let book of booksToRead) {
+        let bookIndex = booksToRead.indexOf(book);
         // create a table row to store book info and add it to the table
         let row = document.createElement("tr");
         // add a data-row-num atribute to the row with the index of the element in an array
-        row.setAttribute("data-rowNum", bookIndex);
-        table.appendChild(row);
+        // TODO: differentiate the data-readnum and data-toreadnum 
+        row.setAttribute("data-to-read-row-num", bookIndex);
+        booksToReadPlaceholder.appendChild(row);
         // for every property in a book object
+        for (let property in book) {
+            let cell = document.createElement("td");
+            cell.textContent = book[property];
+            row.appendChild(cell);
+        }
+        createDeleteButton(bookIndex, row);
+    }
+    for (let book of booksRead) {
+        let bookIndex = booksRead.indexOf(book);
+        let row = document.createElement("tr");
+        row.setAttribute("data-read-row-num", bookIndex);
+        booksReadPlaceholder.appendChild(row);
+
         for (let property in book) {
             let cell = document.createElement("td");
             cell.textContent = book[property];
@@ -61,7 +81,7 @@ function createDeleteButton(bookIndex, row) {
     // create a button give and it a data-row-num attribute associated with the row that it's in
     // append the button to the placeholder
     let deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("data-rowNum", bookIndex);
+    deleteBtn.setAttribute("data-rownum", bookIndex);
     deleteBtn.classList.add("delete-btn");
     deleteBtn.textContent = "Delete";
     deleteBtnPlaceholder.appendChild(deleteBtn);
@@ -69,11 +89,14 @@ function createDeleteButton(bookIndex, row) {
 
 // function for cleaning the table to display the current state of the table
 function clearTable() {
-    const tableBody = document.getElementById("display-books");
+    const tableBodies = document.querySelectorAll(".display-books");
+    // TODO: needs to be for each
     // while tableBody has children, remove the first one
-    while (tableBody.childNodes.length) {
-        tableBody.removeChild(tableBody.childNodes[0]);
-    }
+    tableBodies.forEach((tableBody) => {
+        while (tableBody.childNodes.length) {
+            tableBody.removeChild(tableBody.childNodes[0]);
+        }
+    })
 }
 
 // when the submit button on the form is clicked
@@ -84,14 +107,31 @@ const submitBtn = document.getElementById("add-book");
 submitBtn.addEventListener('click', () => {
     addBookToLibrary();
     clearTable();
-    displayBooks(myLibrary);
+    displayBooks(booksRead, booksToRead);
 })
 
 // onclick function for all the delete-btn
 // it removes the row from the table which has the same data-row-num attribute
 // as the clicked delete button
-let deleteButtonsAll = document.querySelectorAll(".delete-btn");
-deleteButtonsAll.forEach((button) => button.addEventListener('click', () => {
-    console.log("Works!");
-}))
+const booksToReadTableBody = document.querySelector(".display-books-to-read");
+booksToReadTableBody.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete-btn')) {
+        let rowToRemove = document.querySelector(`[data-to-read-row-num="${event.target.dataset.rownum}"]`);
+        booksToReadTableBody.removeChild(rowToRemove);
+        // remove the book object displayed in the given row from myLibrary array to prevent it from displaying while adding another book
+        booksToRead.splice(event.target.dataset.rownum, 1);
+    }
+})
+
+const booksReadTableBody = document.querySelector(".display-books-read");
+booksReadTableBody.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete-btn')) {
+        let rowToRemove = document.querySelector(`[data-read-row-num="${event.target.dataset.rownum}"]`);
+        booksReadTableBody.removeChild(rowToRemove);
+        // remove the book object displayed in the given row from myLibrary array to prevent it from displaying while adding another book
+        booksRead.splice(event.target.dataset.rownum, 1);
+    }
+})
+
+
 
